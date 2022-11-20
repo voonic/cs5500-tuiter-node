9ddc6159e00fd7a8664507d37fe392289b42f9a0
Saket Kumar <kumar.sak@northeastern.edu>
2022-11-20 16:30:47 -0500

    Added documentation to the auth controller

--- controllers/auth-controller.ts
+++ controllers/auth-controller.ts
@@ -3,12 +3,26 @@ import { Express, Request, Response } from "express";
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+/**
+ * An authentication controller that provides
+ * methods to register, login and logout users.
+ * 
+ * @param app The express app on which the controller will run.
+ */
 const AuthenticationController = (app: Express) => {
 
   const userDao: UserDao = new UserDao();
 
+  /**
+   * Responsible for signinup the user.
+   * 
+   * @param req Request containing details of the user.
+   * @param res Response being sent to user
+   * @returns Newly created user to end user.
+   */
   const signup = async (req: Request, res: Response) => {
     const newUser = req.body;
+    console.log(newUser);
     const password = newUser.password;
     const hash = await bcrypt.hash(password, saltRounds);
     newUser.password = hash;
@@ -28,6 +42,14 @@ const AuthenticationController = (app: Express) => {
     }
   }
 
+  /**
+   * Responsible for getting the profile of the user.
+   * If user is not logged in then returns unauthorized error.
+   * 
+   * @param req Request containing details of the user.
+   * @param res Response being sent to user
+   * @returns Newly created user to end user.
+   */
   const profile = (req: Request, res: Response) => {
     //@ts-ignore
     const profile = req.session['profile'];
@@ -39,12 +61,25 @@ const AuthenticationController = (app: Express) => {
     }
   }
 
+  /**
+   * Responsible for loggin out the user.
+   * 
+   * @param req Request containing details of the user.
+   * @param res Response being sent to user
+   */
   const logout = (req: Request, res: Response) => {
     //@ts-ignore
     req.session.destroy();
     res.sendStatus(200);
   }
 
+  /**
+   * Responsible for sign in the user.
+   * 
+   * @param req Request containing details of the user.
+   * @param res Response being sent to user
+   * @returns Existing  user to the frontend.
+   */
   const login = async (req: Request, res: Response) => {
     const user = req.body;
     const username = user.username;
